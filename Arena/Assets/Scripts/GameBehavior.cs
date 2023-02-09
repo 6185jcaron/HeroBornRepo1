d30c5737dc490896313933a7aec52ad14e4ad010
6185jcaron <123214297+6185jcaron@users.noreply.github.com>
2023-02-09 16:11:10 -0700

    updated game behavior

--- Arena/Assets/Scripts/GameBehavior.cs
+++ Arena/Assets/Scripts/GameBehavior.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.SceneManagement;
 public class GameBehavior : MonoBehaviour
 {
     public string labelText = "Collect all 4 items and win your freedom!";
     public int maxItems = 4;
+    public bool showWinScreen = false;
 
     private int _itemsCollected = 0;
     public int Items
@@ -18,6 +20,8 @@ public class GameBehavior : MonoBehaviour
             if(_itemsCollected>=maxItems)
             {
                 labelText = "You've found all the items!";
+                showWinScreen = true;
+                Time.timeScale = 0f;
             }
             else
             {
@@ -42,6 +46,15 @@ public class GameBehavior : MonoBehaviour
         GUI.Box(new Rect(20, 20, 150, 25), "Player Health:" + _playerHP);
         GUI.Box(new Rect(20, 50, 150, 25), "Items Collected: " + _itemsCollected);
         GUI.Label(new Rect(Screen.width / 2 -100, Screen.height - 50, 300, 50), labelText);
+        if (showWinScreen)
+        {
+            if (GUI.Button(new Rect(Screen.width/2 - 100, Screen.height/2 - 50, 200, 100), "YOU WON!"))
+            {
+                SceneManager.LoadScene(0);
+                Time.timeScale = 1.0f;
+            }
+
+        }
        
     }
     // Start is called before the first frame update
