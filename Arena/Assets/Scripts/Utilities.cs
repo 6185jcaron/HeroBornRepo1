dc1b8921495de4281e244c129ed8a02449b47db3
6185jcaron <123214297+6185jcaron@users.noreply.github.com>
2023-02-23 19:54:10 -0700

    added utilities

---
+++ Arena/Assets/Scripts/Utilities.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public static class Utilities
+{
+    public static int playerDeaths = 0;
+    public static void RestartLevel()
+    {
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1.0f;
+    }
+}
+public class Utilities : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
